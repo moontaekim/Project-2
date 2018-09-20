8952e8df8e416e74fd7d5cafdd8797794a9bf9ac
Moon-Tae Kim <moon-tae@Moon-Taes-MacBook-Air.local>
2018-09-20 10:27:15 -0400

    guitarists show on index page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const { Guitarist } = require('../db/schema')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', (req, res) => {
+  Guitarist.find()
+    .then((guitarists) => {
+      res.render('index', {guitarists})
+    })
+})
 
 module.exports = router;
