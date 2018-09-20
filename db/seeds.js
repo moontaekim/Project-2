b72ebedf88b33fb61e850410f38e90a299609e30
Moon-Tae Kim <moon-tae@Moon-Taes-MacBook-Air.local>
2018-09-20 10:40:56 -0400

    img shows for guitarist1

--- db/seeds.js
+++ db/seeds.js
@@ -2,34 +2,36 @@
 require('dotenv').config()
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/gear-tracker', { useNewUrlParser: true})
+//mongoose.connect('mongodb://localhost/gear-tracker', { useNewUrlParser: true})
 //what is going on here?
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true})
 
 const Schema = require('./schema')
 
 const { Guitarist, Song, Gear } = Schema
-
+//gears
 const gearS1 = new Gear({
     name: "Distortion",
     brand: "BOSS",
     description: "distortion pedal heard in Bodysnatchers"
 })
-
+//songs
 const jS1 = new Song({
     name: "Bodysnatchers",
     album: "In Rainbows",
     length: "4:02",
     gear: [gearS1]
 })
-
+//guitarists
 const guitaristOne = new Guitarist({
     name: "Johnny Greenwood",
     band: "Radiohead",
     bio: "Johnny Greenwood master of effects",
-    img: "IMG WILL GO HERE",
+    img: "https://ksassets.timeincuk.net/wp/uploads/sites/55/2017/05/JonnyGreenwoodGettyImages-173087304-1-920x584.jpg",
     song: [jS1]
 })
+
+
 
 Guitarist.deleteMany()
     .then(() => {
