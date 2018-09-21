878a4206a6c1bc4b2aada1d3c330bb25cffff760
Moon-Tae Kim <moon-tae@Moon-Taes-MacBook-Air.local>
2018-09-20 22:50:16 -0400

    edit guitarist form works and updates

--- routes/guitarists.js
+++ routes/guitarists.js
@@ -16,11 +16,19 @@ router.get('/new', (req, res) => {
   res.render('guitarists/new')
 })
 
+//edit
+router.get('/:guitaristId/edit', (req, res) => {
+  Guitarist.findById(req.params.guitaristId)
+    .then((guitarist) => {
+      res.render('guitarists/edit', {guitarist})
+    })
+})
+
 //create
 router.post('/', (req, res) => {
   Guitarist.create(req.body)
     .then((guitarist) => {
-      res.redirect(`/guitarists/${guitarist.id}/songs`)
+      res.redirect(`/guitarists/${guitarist._id}/songs`)
     })
 })
 
@@ -35,8 +43,13 @@ router.get('/:guitaristId/songs', (req, res) => {
   })
 })
 
-
-
+//update
+router.put('/:guitaristId', (req, res) => {
+  Guitarist.findByIdAndUpdate(req.params.guitaristId, req.body)
+    .then(() => {
+      res.redirect('/guitarists')
+    })
+})
 
 //delete
 router.delete('/:guitaristId', (req ,res) => {
